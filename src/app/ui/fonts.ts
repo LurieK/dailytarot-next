c02ee5872ebd15d6b06ce3721b666bf5a74c1a15
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2024-01-02 18:21:52 -0500

    set heading for page.tsx
    imported fonts Almendra, and Arim0

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Almendra, Arimo } from 'next/font/google';
+
+ 
+export const almendra = Almendra({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+
+});
+export const arimo = Arimo({ subsets: ['latin'] })
