cbb5e965a6fad4d2df52acc3e1fae34cd7f33b64
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2024-01-02 18:10:07 -0500

    add cards object array to cards_data file

---
+++ public/cards_array.ts
@@ -0,0 +1,400 @@
+export interface Cards {
+    displayText: string; 
+    imageUrl: string;
+    value: string
+}
+
+export const tarotCards: Cards[] = [
+  {
+    displayText: "The Fool",
+    value: "fool",
+    imageUrl: "./card_images/sm_RWSa-T-00.png",
+  },
+  {
+    displayText: "The Magician I",
+    value: "magician",
+    imageUrl: "./card_images/sm_RWSa-T-01.png",
+  },
+  {
+    displayText: "The High Priestess II",
+    value: "priestess",
+    imageUrl: "./card_images/sm_RWSa-T-02.png",
+  },
+  {
+    displayText: "The Empress III",
+    value: "empress",
+    imageUrl: "./card_images/sm_RWSa-T-03.png",
+  },
+  {
+    displayText: "The Emperor IV",
+    value: "emperor",
+    imageUrl: "./card_images/sm_RWSa-T-04.png",
+  },
+  {
+    displayText: "The Heirophant V",
+    value: "heirophant",
+    imageUrl: "./card_images/sm_RWSa-T-05.png",
+  },
+  {
+    displayText: "The Lovers VI",
+    value: "lovers",
+    imageUrl: "./card_images/sm_RWSa-T-06.png",
+  },
+  {
+    displayText: "The Chariot VII",
+    value: "chariot",
+    imageUrl: "./card_images/sm_RWSa-T-07.png",
+  },
+  {
+    displayText: "Strength VIII",
+    value: "strength",
+    imageUrl: "./card_images/sm_RWSa-T-08.png",
+  },
+  {
+    displayText: "The Hermit IX",
+    value: "hermit",
+    imageUrl: "./card_images/sm_RWSa-T-09.png",
+  },
+  {
+    displayText: "The Wheel of Fortune X",
+    value: "wheel",
+    imageUrl: "./card_images/sm_RWSa-T-10.png",
+  },
+  {
+    displayText: "Justice XI",
+    value: "justice",
+    imageUrl: "./card_images/sm_RWSa-T-11.png",
+  },
+  {
+    displayText: "The Hanged Man XII",
+    value: "hanged",
+    imageUrl: "./card_images/sm_RWSa-T-12.png",
+  },
+  {
+    displayText: "Death XIII",
+    value: "death",
+    imageUrl: "./card_images/sm_RWSa-T-13.png",
+  },
+  {
+    displayText: "Temprence XIV",
+    value: "temperance",
+    imageUrl: "./card_images/sm_RWSa-T-14.png",
+  },
+  {
+    displayText: "The Devil XV",
+    value: "devil",
+    imageUrl: "./card_images/sm_RWSa-T-15.png",
+  },
+  {
+    displayText: "The Tower XVI",
+    value: "tower",
+    imageUrl: "./card_images/sm_RWSa-T-16.png",
+  },
+  {
+    displayText: "The Star XVII",
+    value: "star",
+    imageUrl: "./card_images/sm_RWSa-T-17.png",
+  },
+  {
+    displayText: "The Moon XVIII",
+    value: "moon",
+    imageUrl: "./card_images/sm_RWSa-T-18.png",
+  },
+  {
+    displayText: "The Sun XIX",
+    value: "sun",
+    imageUrl: "./card_images/sm_RWSa-T-19.png",
+  },
+  {
+    displayText: "Judgement XX",
+    value: "judgement",
+    imageUrl: "./card_images/sm_RWSa-T-20.png",
+  },
+  {
+    displayText: "The World XXI",
+    value: "world",
+    imageUrl: "./card_images/sm_RWSa-T-21.png",
+  },
+  {
+    displayText: "Ace of Swords",
+    value: "as",
+    imageUrl: "./card_images/sm_RWSa-S-0A.png",
+  },
+  {
+    displayText: "Two of Swords",
+    value: "2s",
+    imageUrl: "./card_images/sm_RWSa-S-02.png",
+  },
+  {
+    displayText: "Three of Swords",
+    value: "3s",
+    imageUrl: "./card_images/sm_RWSa-S-03.png",
+  },
+  {
+    displayText: "Four of Swords",
+    value: "4s",
+    imageUrl: "./card_images/sm_RWSa-S-04.png",
+  },
+  {
+    displayText: "Five of Swords",
+    value: "5s",
+    imageUrl: "./card_images/sm_RWSa-S-05.png",
+  },
+  {
+    displayText: "Six of Swords",
+    value: "6s",
+    imageUrl: "./card_images/sm_RWSa-S-06.png",
+  },
+  {
+    displayText: "Seven of Swords",
+    value: "7s",
+    imageUrl: "./card_images/sm_RWSa-S-07.png",
+  },
+  {
+    displayText: "Eight of Swords",
+    value: "8s",
+    imageUrl: "./card_images/sm_RWSa-S-08.png",
+  },
+  {
+    displayText: "Nine of Swords",
+    value: "9s",
+    imageUrl: "./card_images/sm_RWSa-S-09.png",
+  },
+  {
+    displayText: "Ten of Swords",
+    value: "10s",
+    imageUrl: "./card_images/sm_RWSa-S-10.png",
+  },
+  {
+    displayText: "Page of Swords",
+    value: "ps",
+    imageUrl: "./card_images/sm_RWSa-S-J1.png",
+  },
+  {
+    displayText: "Knight of Swords",
+    value: "kns",
+    imageUrl: "./card_images/sm_RWSa-S-J2.png",
+  },
+  {
+    displayText: "Queen of Swords",
+    value: "qs",
+    imageUrl: "./card_images/sm_RWSa-S-QU.png",
+  },
+  {
+    displayText: "King of Swords",
+    value: "ks",
+    imageUrl: "./card_images/sm_RWSa-S-KI.png",
+  },
+  {
+    displayText: "Ace of Wands",
+    value: "aw",
+    imageUrl: "./card_images/sm_RWSa-W-0A.png",
+  },
+  {
+    displayText: "Two of Wands",
+    value: "2w",
+    imageUrl: "./card_images/sm_RWSa-W-02.png",
+  },
+  {
+    displayText: "Three of Wands",
+    value: "3w",
+    imageUrl: "./card_images/sm_RWSa-W-03.png",
+  },
+  {
+    displayText: "Four of Wands",
+    value: "4w",
+    imageUrl: "./card_images/sm_RWSa-W-04.png",
+  },
+  {
+    displayText: "Five of Wands",
+    value: "5w",
+    imageUrl: "./card_images/sm_RWSa-W-05.png",
+  },
+  {
+    displayText: "Six of Wands",
+    value: "6w",
+    imageUrl: "./card_images/sm_RWSa-W-06.png",
+  },
+  {
+    displayText: "Seven of Wands",
+    value: "7w",
+    imageUrl: "./card_images/sm_RWSa-W-07.png",
+  },
+  {
+    displayText: "Eight of Wands",
+    value: "8w",
+    imageUrl: "./card_images/sm_RWSa-W-08.png",
+  },
+  {
+    displayText: "Nine of Wands",
+    value: "9w",
+    imageUrl: "./card_images/sm_RWSa-W-09.png",
+  },
+  {
+    displayText: "Ten of Wands",
+    value: "10w",
+    imageUrl: "./card_images/sm_RWSa-W-10.png",
+  },
+  {
+    displayText: "Page of Wands",
+    value: "pw",
+    imageUrl: "./card_images/sm_RWSa-W-J1.png",
+  },
+  {
+    displayText: "Knight of Wands",
+    value: "knw",
+    imageUrl: "./card_images/sm_RWSa-W-J2.png",
+  },
+  {
+    displayText: "Queen of Wands",
+    value: "qw",
+    imageUrl: "./card_images/sm_RWSa-W-QU.png",
+  },
+  {
+    displayText: "King of Wands",
+    value: "kw",
+    imageUrl: "./card_images/sm_RWSa-W-KI.png",
+  },
+  {
+    displayText: "Ace of Pentacles",
+    value: "ap",
+    imageUrl: "./card_images/sm_RWSa-P-0A.png",
+  },
+  {
+    displayText: "Two of Pentacles",
+    value: "2p",
+    imageUrl: "./card_images/sm_RWSa-P-02.png",
+  },
+  {
+    displayText: "Three of Pentacles",
+    value: "3p",
+    imageUrl: "./card_images/sm_RWSa-P-03.png",
+  },
+  {
+    displayText: "Four of Pentacles",
+    value: "4p",
+    imageUrl: "./card_images/sm_RWSa-P-04.png",
+  },
+  {
+    displayText: "Five of Pentacles",
+    value: "5p",
+    imageUrl: "./card_images/sm_RWSa-P-05.png",
+  },
+  {
+    displayText: "Six of Pentacles",
+    value: "6p",
+    imageUrl: "./card_images/sm_RWSa-P-06.png",
+  },
+  {
+    displayText: "Seven of Pentacles",
+    value: "7p",
+    imageUrl: "./card_images/sm_RWSa-P-07.png",
+  },
+  {
+    displayText: "Eight of Pentacles",
+    value: "8p",
+    imageUrl: "./card_images/sm_RWSa-P-08.png",
+  },
+  {
+    displayText: "Nine of Pentacles",
+    value: "9p",
+    imageUrl: "./card_images/sm_RWSa-P-09.png",
+  },
+  {
+    displayText: "Ten of Pentacles",
+    value: "10p",
+    imageUrl: "./card_images/sm_RWSa-P-10.png",
+  },
+  {
+    displayText: "Page of Pentacles",
+    value: "pp",
+    imageUrl: "./card_images/sm_RWSa-P-J1.png",
+  },
+  {
+    displayText: "Knight of Pentacles",
+    value: "knp",
+    imageUrl: "./card_images/sm_RWSa-P-J2.png",
+  },
+  {
+    displayText: "Queen of Pentacles",
+    value: "qp",
+    imageUrl: "./card_images/sm_RWSa-P-QU.png",
+  },
+  {
+    displayText: "King of Pentacles",
+    value: "kp",
+    imageUrl: "./card_images/sm_RWSa-P-KI.png",
+  },
+  {
+    displayText: "Ace of Cups",
+    value: "ac",
+    imageUrl: "./card_images/sm_RWSa-C-0A.png",
+  },
+  {
+    displayText: "Two of Cups",
+    value: "2c",
+    imageUrl: "./card_images/sm_RWSa-C-02.png",
+  },
+  {
+    displayText: "Three of Cups",
+    value: "3c",
+    imageUrl: "./card_images/sm_RWSa-C-03.png",
+  },
+  {
+    displayText: "Four of Cups",
+    value: "4c",
+    imageUrl: "./card_images/sm_RWSa-C-04.png",
+  },
+  {
+    displayText: "Five of Cups",
+    value: "5c",
+    imageUrl: "./card_images/sm_RWSa-C-05.png",
+  },
+  {
+    displayText: "Six of Cups",
+    value: "6c",
+    imageUrl: "./card_images/sm_RWSa-C-06.png",
+  },
+  {
+    displayText: "Seven of Cups",
+    value: "7c",
+    imageUrl: "./card_images/sm_RWSa-C-07.png",
+  },
+  {
+    displayText: "Eight of Cups",
+    value: "8c",
+    imageUrl: "./card_images/sm_RWSa-C-08.png",
+  },
+  {
+    displayText: "Nine of Cups",
+    value: "9c",
+    imageUrl: "./card_images/sm_RWSa-C-09.png",
+  },
+  {
+    displayText: "Ten of Cups",
+    value: "10c",
+    imageUrl: "./card_images/sm_RWSa-C-10.png",
+  },
+  {
+    displayText: "Page of Cups",
+    value: "pc",
+    imageUrl: "./card_images/sm_RWSa-C-J1.png",
+  },
+  {
+    displayText: "Knight of Cups",
+    value: "knc",
+    imageUrl: "./card_images/sm_RWSa-C-J2.png",
+  },
+  {
+    displayText: "Queen of Cups",
+    value: "qc",
+    imageUrl: "./card_images/sm_RWSa-C-QU.png",
+  },
+  {
+    displayText: "King of Cups",
+    value: "kc",
+    imageUrl: "./card_images/sm_RWSa-C-KI.png",
+  },
+];
+
+
